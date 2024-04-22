fd52875820a45f7b0993434490b39b89137fdbeb
Walisson Gomes <walissonwaal@gmail.com>
2024-04-22 15:02:03 -0300

    Adiciona responsividade

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '76e2e53bf0547cb4810e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '445094339ad33d140fd4');
